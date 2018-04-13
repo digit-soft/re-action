a2e1a13d801e9420ef4553584d4494113bd587ee
Digit <digit.vova@gmail.com>
2018-04-13 18:00:21 +0300

    Add basic JsExpression

---
+++ src/Base/JsExpression.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Reaction\Base;
+
+/**
+ * JsExpression marks a string as a JavaScript expression.
+ *
+ * When using [[\Reaction\Helpers\Json::encode()]] or [[\Reaction\Helpers\Json::htmlEncode()]] to encode a value, JsonExpression objects
+ * will be specially handled and encoded as a JavaScript expression instead of a string.
+ */
+class JsExpression extends BaseObject
+{
+    /**
+     * @var string the JavaScript expression represented by this object
+     */
+    public $expression;
+
+
+    /**
+     * Constructor.
+     * @param string $expression the JavaScript expression represented by this object
+     * @param array $config additional configurations for this object
+     */
+    public function __construct($expression, $config = [])
+    {
+        $this->expression = $expression;
+        parent::__construct($config);
+    }
+
+    /**
+     * The PHP magic function converting an object into a string.
+     * @return string the JavaScript expression.
+     */
+    public function __toString()
+    {
+        return $this->expression;
+    }
+}
