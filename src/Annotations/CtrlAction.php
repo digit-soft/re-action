8c2f3a501ecc6a09eb9be77451a6055f309158e3
Digit <digit.vova@gmail.com>
2018-04-13 15:25:40 +0300

    Changes in Annotations

--- src/Annotations/CtrlAction.php
+++ src/Annotations/CtrlAction.php
@@ -2,18 +2,49 @@
 
 namespace Reaction\Annotations;
 
+use Doctrine\Common\Annotations\Annotation\Attribute;
+use Doctrine\Common\Annotations\Annotation\Attributes;
+use Doctrine\Common\Annotations\Annotation\Required;
+use Doctrine\Common\Annotations\Annotation\Target;
+
 /**
  * @Annotation
- * @Target({"METHOD","PROPERTY"})
+ * @Target({"METHOD"})
+ * @Attributes({
+ *   @Attribute("path", type = "string"),
+ *   @Attribute("method",  type = "array"),
+ * })
  *
  * Some Annotation using a constructor
  */
 class CtrlAction
 {
-    public $data;
+    /** @var array Http method */
+    public $method = ['GET'];
 
+    /**
+     * @Required
+     * @var string Action path
+     */
+    public $path;
+
+    /**
+     * CtrlAction constructor.
+     * @param array $values
+     * @throws \Exception
+     */
     public function __construct($values = [])
     {
-        \Reaction::$app->logger->warning($values);
+        if(count($values) === 1 && isset($values['value'])) $values = $values['value'];
+        if(is_string($values)) {
+            $this->path = $values;
+        } elseif (is_array($values)) {
+            if(!empty($values['method'])) $this->method = (array)$values['method'];
+            if(!empty($values['path'])) $this->path = (string)$values['path'];
+        }
+
+        if(empty($this->path)) {
+            throw new \Exception("Property 'path' is required");
+        }
     }
 }
