0bcd33d21640874f4763b8b78ca5f81d61533bc6
Digit <digit.vova@gmail.com>
2018-04-20 18:01:27 +0300

    Add static render method

--- src/Base/View.php
+++ src/Base/View.php
@@ -542,4 +542,31 @@ class View extends Component implements DynamicContentAwareInterface
         $this->emit(self::EVENT_END_PAGE, [$this]);
         ob_end_flush();
     }
+
+    public static function renderPhpStateless($_file_, $_params_ = [])
+    {
+        $_file_ = Reaction::$app->getAlias($_file_);
+        $_obInitialLevel_ = ob_get_level();
+        ob_start();
+        ob_implicit_flush(false);
+        extract($_params_, EXTR_OVERWRITE);
+        try {
+            require $_file_;
+            return ob_get_clean();
+        } catch (\Exception $e) {
+            while (ob_get_level() > $_obInitialLevel_) {
+                if (!@ob_end_clean()) {
+                    ob_clean();
+                }
+            }
+            throw $e;
+        } catch (\Throwable $e) {
+            while (ob_get_level() > $_obInitialLevel_) {
+                if (!@ob_end_clean()) {
+                    ob_clean();
+                }
+            }
+            throw $e;
+        }
+    }
 }
