5e60d7114e83867da4334bd9cd443585394c2d8d
Digit <digit.vova@gmail.com>
2018-04-26 11:11:56 +0300

    Add Request I18nGroup with formatter

---
+++ src/I18n/Request/I18nGroup.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Reaction\I18n\Request;
+
+use Reaction\Web\RequestService;
+
+/**
+ * Class I18nGroup
+ * @package Reaction\I18n\Request
+ * @property Formatter $formatter
+ */
+class I18nGroup extends RequestService
+{
+}
