2558f7636e0a1b11d3eef363973a0017a1c9a546
Digit <digit.vova@gmail.com>
2018-04-27 14:41:07 +0300

    CachedSessionHandler changes

--- src/Web/Sessions/CachedSessionHandler.php
+++ src/Web/Sessions/CachedSessionHandler.php
@@ -2,12 +2,11 @@
 
 namespace Reaction\Web\Sessions;
 
-use React\Cache\CacheInterface;
 use React\Promise\ExtendedPromiseInterface;
-use Reaction\Base\Component;
-use Reaction\Helpers\Json;
+use React\Promise\PromiseInterface;
+use Reaction\Cache\ExtendedCacheInterface;
+use Reaction\Exceptions\SessionException;
 use Reaction\Promise\Promise;
-use Reaction\Web\AppRequestInterface;
 
 /**
  * Class CachedSessionHandler
@@ -15,95 +14,156 @@ use Reaction\Web\AppRequestInterface;
  */
 class CachedSessionHandler extends SessionHandlerAbstract
 {
-    /** @var string|CacheInterface */
+    /** @var string|ExtendedCacheInterface */
     public $cache = 'arrayCacheDefault';
     /** @var string Key prefix for storage */
     public $keyPrefix = 'sess_';
 
-    public $dataKey = 'data';
-
-    public $timestampKey = 'ts';
-
-    public function init()
-    {
-        if (!is_object($this->cache)) {
-           $this->cache = \Reaction::create($this->cache);
-        }
-        parent::init();
-    }
+    /** @var string Array key where user data is located */
+    public $dataKey = '_data';
+    /** @var string Array key where timestamp is located */
+    public $timestampKey = '_ts';
+    /** @var array Session keys with timestamp */
+    protected $keys = [];
 
     /**
      * Read session data and returns serialized|encoded data
-     * @param string $sessionId
-     * @return ExtendedPromiseInterface
+     * @param string $id
+     * @return PromiseInterface  with session data
      */
-    public function read($sessionId)
+    public function read($id)
     {
+        $key = $this->getSessionKey($id);
         $self = $this;
-        $key = $this->keyPrefix . $sessionId;
         return $this->getDataFromCache($key)->then(
             function ($record) use ($self) {
                 return $self->extractData($record, true);
             },
-            function (\Throwable $e) {
-                if (\Reaction::isDebug()) {
-                    \Reaction::$app->logger->error($e->getMessage());
-                }
-                return [];
+            function ($error = null) use ($id) {
+                $message = sprintf('Failed to read session data for "%s"', $id);
+                throw new SessionException($message, 0, $error);
             }
         );
     }
 
     /**
      * Write session data to storage
-     * @param string $sessionId
+     * @param string $id
      * @param array  $data
-     * @return ExtendedPromiseInterface
+     * @return PromiseInterface  with session data
      */
-    public function write($sessionId, $data)
+    public function write($id, $data)
     {
+        $key = $this->getSessionKey($id);
         $self = $this;
-        $key = $this->keyPrefix . $sessionId;
-        $record = [ ];
-        $record[$this->timestampKey] = time();
-        $record[$this->dataKey] = $data;
-        return $this->writeDataToCache($key, $record);
+        $record = $this->packRecord($data);
+        return $this->writeDataToCache($key, $record)->then(
+            function () use ($self, $key, $id) {
+                $self->keys[$id] = time();
+                return $self->cache->get($key);
+            },
+            function ($error = null) use ($id) {
+                $message = sprintf('Failed to write session data for "%s"', $id);
+                throw new SessionException($message, 0, $error);
+            }
+        );
     }
 
     /**
      * Destroy a session
-     * @param string $sessionId The session ID being destroyed.
-     * @return ExtendedPromiseInterface
+     * @param string $id The session ID being destroyed.
+     * @return PromiseInterface  with bool when finished
      */
-    public function destroy($sessionId)
+    public function destroy($id)
     {
-        // TODO: Implement destroy() method.
+        if (isset($this->keys[$id])) {
+            unset($this->keys[$id]);
+        }
+        return $this->cache->remove($id)->then(
+            function () use ($id) { return $id; },
+            function ($error = null) use ($id) {
+                $message = sprintf('Failed to destroy session "%s"', $id);
+                throw new SessionException($message, 0, $error);
+            }
+        );
     }
 
-//    /**
-//     * Cleanup old sessions. Timer callback.
-//     * Sessions that have not updated for the last '$this->gcLifetime' seconds will be removed.
-//     * @return void
-//     */
-//    public function gc()
-//    {
-//        // TODO: Implement gc() method.
-//    }
+    /**
+     * Cleanup old sessions. Timer callback.
+     * Sessions that have not updated for the last '$this->gcLifetime' seconds will be archived.
+     * Sessions in archive with age bigger than '$this->sessionLifetime' seconds will be removed.
+     * @see $gcLifetime
+     * @see $sessionLifetime
+     * @return void
+     */
+    public function gc()
+    {
+        \Reaction::$app->logger->info($this->keys);
+        if ($this->_gcIsRunning || empty($this->keys)) {
+            return;
+        }
+        $this->_gcIsRunning = true;
+        $expiredTime = time() - $this->gcLifetime;
+        $self = $this;
+        $promises = [];
+        foreach ($this->keys as $id => $ts) {
+            if ($ts > $expiredTime) {
+                continue;
+            }
+            $promise = $this->read($id)->then(
+                function ($data) use ($self, $id) {
+                    return $self->archiveSessionData($id, $data);
+                }
+            )->then(
+                function () use ($self, $id) {
+                    return $self->destroy($id);
+                }
+            );
+            $promises[] = $promise;
+        }
+        if (!empty($promises)) {
+            \Reaction\Promise\all($promises)->always(
+                function () use ($self) { $self->_gcIsRunning = false; }
+            );
+        } else {
+            $this->_gcIsRunning = false;
+        }
+    }
 
     /**
      * Update timestamp of a session
-     * @param string $sessionId The session id
-     * @param string $data
+     * @param string $id The session id
+     * @param array  $data
      * The encoded session data. This data is the
      * result of the PHP internally encoding
      * the $_SESSION superglobal to a serialized
      * string and passing it as this parameter.
-     * @return bool
+     * @return PromiseInterface  with bool when finished
      */
-    public function updateTimestamp($sessionId, $data)
+    public function updateTimestamp($id, $data = null)
     {
-        // TODO: Implement updateTimestamp() method.
+        $self = $this;
+        return $this->read($id)->then(
+            function ($dataStored) use ($self, $id, $data) {
+                if (isset($data)) {
+                    $dataStored = $data;
+                }
+                return $self->write($id, $dataStored);
+            }
+        )->then(
+            function () { return true; }
+        );
     }
+
+    /**
+     * Get cache session key
+     * @param string $sessionId
+     * @return string
+     */
+    protected function getSessionKey($sessionId) {
+        return $this->keyPrefix . trim($sessionId);
+    }
+
 
     /**
      * Extract session data from record
@@ -118,11 +178,11 @@ class CachedSessionHandler extends SessionHandlerAbstract
 
     /**
      * Extract session timestamp from record
-     * @param array $sessionRecord
+     * @param array $record
      * @return integer|null
      */
-    public function extractTimestamp($sessionRecord = []) {
-        return isset($sessionRecord[$this->timestampKey]) ? $sessionRecord[$this->timestampKey] : null;
+    public function extractTimestamp($record = []) {
+        return isset($record[$this->timestampKey]) ? $record[$this->timestampKey] : null;
     }
 
     /**
@@ -137,11 +197,9 @@ class CachedSessionHandler extends SessionHandlerAbstract
                 function ($data) use ($r, $c) {
                     $r($data);
                 },
-                function (\Throwable $error) use ($r, $c) {
-                    if (\Reaction::isDebug()) {
-                        \Reaction::$app->logger->error($error->getMessage());
-                    }
-                    $r(null);
+                function ($error = null) use ($c, $key) {
+                    $message = sprintf('Failed to get session data from cache for "%s"', $key);
+                    $c(new SessionException($message));
                 }
             );
         }));
@@ -149,23 +207,46 @@ class CachedSessionHandler extends SessionHandlerAbstract
 
     /**
      * @param string $key
-     * @param array  $sessionRecord
-     * @return Promise
+     * @param array  $record
+     * @return ExtendedPromiseInterface
      */
-    protected function writeDataToCache($key, $sessionRecord) {
-        $data = $this->extractData($sessionRecord);
-        $ts = $this->extractTimestamp($sessionRecord);
-        if (null === $ts) {
-            $ts = time();
-        }
-        $record = [];
-        $record[$this->dataKey] = $this->serializeSessionData($data);
-        $record[$this->timestampKey] = $ts;
+    protected function writeDataToCache($key, $record) {
+        $ts = $this->extractTimestamp($record);
+        $record[$this->timestampKey] = isset($ts) ? $ts : time();
 
         $cache = $this->cache;
-        return (new Promise(function ($r, $c) use ($cache, $key, $record) {
-            $cache->set($key, $record);
-            $r(true);
-        }));
+        return new Promise(function ($r, $c) use ($cache, $key, $record) {
+            $cache->set($key, $record)->then(
+                function () use ($r) {
+                    $r(true);
+                },
+                function ($error = null) use ($c, $key) {
+                    $message = sprintf('Failed to write session data to cache for "%s"', $key);
+                    $c(new SessionException($message));
+                }
+            );
+        });
+    }
+
+    /**
+     * Pack record
+     * @param array $data
+     * @return array
+     */
+    protected function packRecord($data) {
+        $record = [];
+        $dKey = $this->dataKey;
+        $tKey = $this->timestampKey;
+        if (isset($data[$tKey])) {
+            unset($data[$tKey]);
+        }
+        if (isset($data[$dKey])) {
+            $record[$dKey] = $data[$dKey];
+        } else {
+            $record[$dKey] = $data;
+        }
+        $record[$this->timestampKey] = time();
+        $record[$dKey] = $this->serializeSessionData($record[$dKey]);
+        return $record;
     }
 }
