aa2ed426637163fac73725191092039d385c10bd
Digit <digit.vova@gmail.com>
2018-05-03 14:27:01 +0300

    default view files changes

--- src/Views/error/view.php
+++ src/Views/error/view.php
@@ -1,8 +1,10 @@
 <?php
-/** @var \Reaction\Web\View $this */
-/** @var \Reaction\Exceptions\Exception $exception */
-/** @var string $exceptionName */
-
+/**
+ * @var \Reaction\Web\View             $this
+ * @var \Reaction\Exceptions\Exception $exception
+ * @var string                         $exceptionName
+ */
+$this->title = 'Error :: ' . $exceptionName;
 ?>
 
 <h1><?= $exceptionName ?></h1>
--- src/Views/layout/main.php
+++ src/Views/layout/main.php
@@ -1,11 +1,15 @@
-<!doctype html>
+<?php
+/**
+ * @var \Reaction\Web\View $this
+ * @var string             $content
+ */
+?><!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title><?= $this->title ?></title>
 </head>
 <body>
 <?= $content; ?>
