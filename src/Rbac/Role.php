bcd7034b4eb662b2c384a097317dd6ac880b744d
Digit <digit.vova@gmail.com>
2018-05-07 18:01:11 +0300

    Add basic Rbac placeholder

---
+++ src/Rbac/Role.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Reaction\Rbac;
+
+/**
+ * For more details and usage information on Role, see the [guide article on security authorization](guide:security-authorization).
+ */
+class Role extends Item
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $type = self::TYPE_ROLE;
+}
---
+++ src/Rbac/migrations/m170907_052038_rbac_add_index_on_auth_assignment_user_id.php
@@ -0,0 +1,48 @@
+<?php
+
+use Reaction\Exceptions\InvalidConfigException;
+use Reaction\Rbac\DbManager;
+use yii\db\Migration;
+
+/**
+ * Adds index on `user_id` column in `auth_assignment` table for performance reasons.
+ *
+ * @see https://github.com/yiisoft/yii2/pull/14765
+ */
+class m170907_052038_rbac_add_index_on_auth_assignment_user_id extends Migration
+{
+    public $column = 'user_id';
+    public $index = 'auth_assignment_user_id_idx';
+
+    /**
+     * @throws Reaction\Exceptions\InvalidConfigException
+     * @return DbManager
+     */
+    protected function getAuthManager()
+    {
+        $authManager = Yii::$app->getAuthManager();
+        if (!$authManager instanceof DbManager) {
+            throw new InvalidConfigException('You should configure "authManager" component to use database before executing this migration.');
+        }
+
+        return $authManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up()
+    {
+        $authManager = $this->getAuthManager();
+        $this->createIndex($this->index, $authManager->assignmentTable, $this->column);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function down()
+    {
+        $authManager = $this->getAuthManager();
+        $this->dropIndex($this->index, $authManager->assignmentTable);
+    }
+}
