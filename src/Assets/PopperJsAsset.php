8e563060fdc13b8ad3d1d004375f28cd21fc4c11
digit <digit.vova@gmail.com>
2018-05-11 17:10:34 +0300

    Add PopperJsAsset

---
+++ src/Assets/PopperJsAsset.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Reaction\Assets;
+
+use Reaction\Web\AssetBundle;
+
+class PopperJsAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/popper.js';
+
+    public $js = [];
+}
