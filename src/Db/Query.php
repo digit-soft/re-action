ac9d29cac123b631266e8719cb5d676f5e21c76e
digit <digit.vova@gmail.com>
2018-05-15 15:30:25 +0300

    Db\Query changes

--- src/Db/Query.php
+++ src/Db/Query.php
@@ -152,68 +152,6 @@ class Query extends Component implements QueryInterface, ExpressionInterface
     }
 
     /**
-     * Starts a batch query.
-     *
-     * A batch query supports fetching data in batches, which can keep the memory usage under a limit.
-     * This method will return a [[BatchQueryResult]] object which implements the [[\Iterator]] interface
-     * and can be traversed to retrieve the data in batches.
-     *
-     * For example,
-     *
-     * ```php
-     * $query = (new Query)->from('user');
-     * foreach ($query->batch() as $rows) {
-     *     // $rows is an array of 100 or fewer rows from user table
-     * }
-     * ```
-     *
-     * @param int               $batchSize the number of records to be fetched in each batch.
-     * @param DatabaseInterface $db the database connection. If not set, the "db" application component will be used.
-     * @return BatchQueryResult the batch query result. It implements the [[\Iterator]] interface
-     * and can be traversed to retrieve the data in batches.
-     * @throws InvalidConfigException
-     */
-    public function batch($batchSize = 100, $db = null)
-    {
-        return \Reaction::create([
-            'class' => BatchQueryResult::className(),
-            'query' => $this,
-            'batchSize' => $batchSize,
-            'db' => $db,
-            'each' => false,
-        ]);
-    }
-
-    /**
-     * Starts a batch query and retrieves data row by row.
-     *
-     * This method is similar to [[batch()]] except that in each iteration of the result,
-     * only one row of data is returned. For example,
-     *
-     * ```php
-     * $query = (new Query)->from('user');
-     * foreach ($query->each() as $row) {
-     * }
-     * ```
-     *
-     * @param int               $batchSize the number of records to be fetched in each batch.
-     * @param DatabaseInterface $db the database connection. If not set, the "db" application component will be used.
-     * @return BatchQueryResult the batch query result. It implements the [[\Iterator]] interface
-     * and can be traversed to retrieve the data in batches.
-     * @throws InvalidConfigException
-     */
-    public function each($batchSize = 100, $db = null)
-    {
-        return \Reaction::create([
-            'class' => BatchQueryResult::className(),
-            'query' => $this,
-            'batchSize' => $batchSize,
-            'db' => $db,
-            'each' => true,
-        ]);
-    }
-
-    /**
      * Executes the query and returns all results as an array.
      * @param DatabaseInterface $db the database connection used to generate the SQL statement.
      * If this parameter is not given, the `db` application component will be used.
