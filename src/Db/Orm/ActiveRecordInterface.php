fb407586eaf0f5a65ea72e19889351874519e59e
digit <digit.vova@gmail.com>
2018-05-18 17:56:28 +0300

    Active

--- src/Db/Orm/ActiveRecordInterface.php
+++ src/Db/Orm/ActiveRecordInterface.php
@@ -459,4 +459,14 @@ interface ActiveRecordInterface extends StaticInstanceInterface
      * @return DatabaseInterface the database connection used by this AR class.
      */
     public static function getDb();
+
+    /**
+     * Declares the name of the database table associated with this AR class.
+     * By default this method returns the class name as the table name by calling [[Inflector::camel2id()]]
+     * with prefix [[Connection::tablePrefix]]. For example if [[Connection::tablePrefix]] is `tbl_`,
+     * `Customer` becomes `tbl_customer`, and `OrderItem` becomes `tbl_order_item`. You may override this method
+     * if the table is not named after this convention.
+     * @return string the table name
+     */
+    public static function tableName();
 }
