1d2bddf413c39c4ac6b5a0a1048c62a844888003
digit <digit.vova@gmail.com>
2018-05-24 15:36:00 +0300

    Add StaticApplicationConsole

---
+++ src/StaticApplicationConsole.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Reaction;
+
+/**
+ * Class StaticApplicationConsole
+ * @package Reaction
+ */
+class StaticApplicationConsole extends StaticApplicationAbstract
+{
+    public function run()
+    {
+        parent::run();
+    }
+}
