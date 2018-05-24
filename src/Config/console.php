ff101d82ae378cb3739b1ce8ff6bb3ee3cd1b39a
digit <digit.vova@gmail.com>
2018-05-24 17:44:54 +0300

    console config changes

--- src/Config/console.php
+++ src/Config/console.php
@@ -2,11 +2,28 @@
 
 /** Default console config */
 return [
+    //Static application config
+    'appStatic' => [
+        'debug' => true,
+        //Components
+        'components' => [
+        ],
+    ],
+    //Request application config
+    'appRequest' => [
+        'components' => [
+            'reqHelper' => [
+                'class' => 'Reaction\Console\Web\RequestHelper',
+            ],
+        ],
+    ],
     'container' => [
         'definitions' => [],
         'singletons' => [
             //Application
             'Reaction\StaticApplicationInterface' => 'Reaction\StaticApplicationConsole',
+            //Router
+            'Reaction\Routes\RouterInterface' => 'Reaction\Console\Routes\Router',
         ],
     ],
 ];
