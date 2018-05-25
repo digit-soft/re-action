251d036b3c75de5910b2a601889cc24fa802acd5
digit <digit.vova@gmail.com>
2018-05-25 17:41:55 +0300

    Console\Routes\Router update

--- src/Console/Routes/Router.php
+++ src/Console/Routes/Router.php
@@ -6,6 +6,7 @@ use FastRoute\Dispatcher;
 use Psr\Http\Message\ServerRequestInterface;
 use React\Promise\PromiseInterface;
 use Reaction;
+use Reaction\Helpers\Inflector;
 use Reaction\RequestApplicationInterface;
 use Reaction\Routes\Controller;
 use Reaction\Routes\RouterAbstract;
@@ -22,6 +23,10 @@ class Router extends RouterAbstract implements RouterInterface
     public $_errorController = [
         'class' => 'Reaction\Console\Routes\ErrorController',
     ];
+    /**
+     * @var string Default controller if none matches
+     */
+    public $defaultController = 'Reaction\Console\Controllers\DefaultController';
 
     /**
      * Register all defined routes in dispatcher
@@ -39,18 +44,27 @@ class Router extends RouterAbstract implements RouterInterface
     {
         $path = (string)$app->reqHelper->getPathInfo();
         list($controllerName, $actionName) = $this->getControllerFromPathInfo($path);
-        if (!isset($controllerName) || !isset($actionName)) {
-            return [Dispatcher::NOT_FOUND];
-        }
-        $controller = $this->searchController($controllerName, $actionName);
+        list($controller, $actionName) = $this->searchController($controllerName, $actionName);
         if (null === $controller) {
-            return [Dispatcher::NOT_FOUND];
+            list($controllerName, $actionName) = $this->getDefaultControllerAndAction();
+            $controller = Reaction::create($controllerName);
         }
         return [Dispatcher::FOUND, [$controller, $actionName]];
     }
 
-    protected function searchController($controllerName, $actionName) {
+    /**
+     * Search controller by name and existing method
+     * @param string $controllerName
+     * @param string $actionName
+     * @return array|null
+     */
+    protected function searchController($controllerName, $actionName)
+    {
+        if (!isset($controllerName) || !isset($actionName)) {
+            return [null, $actionName];
+        }
         $controller = null;
+        $action = $actionName;
         foreach ($this->controllerNamespaces as $namespace) {
             $controllerNameFull = $namespace . '\\' . $controllerName;
             if (!class_exists($controllerNameFull)) {
@@ -60,29 +74,48 @@ class Router extends RouterAbstract implements RouterInterface
                 $reflection = new \ReflectionClass($controllerNameFull);
                 $hasAction = $reflection->hasMethod($actionName);
             } catch (\ReflectionException $exception) {
-                $hasAction = false;
-            }
-            if(!$hasAction) {
                 continue;
             }
+            if(!$hasAction) {
+                $action = 'actionHelp';
+            }
             $controller = Reaction::create($controllerNameFull);
+            break;
         }
-        return $controller;
+        return [$controller, $action];
     }
 
+    /**
+     * Get controller class name (not full) and action from path info
+     * @param string $path
+     * @return array with controller and action names [controller, action]
+     */
     protected function getControllerFromPathInfo($path) {
         $parts = explode('/', $path);
         if (count($parts) < 2) {
-            return [null, null];
+            return $this->getDefaultControllerAndAction();
+        } else {
+            $action = mb_strtolower(array_pop($parts));
+            $action = 'action' . Inflector::id2camel($action);
+            $controller = mb_strtolower(array_pop($parts));
+            $controller = Inflector::id2camel($controller) . 'Controller';
+            if (!empty($parts)) {
+                array_walk($parts, function(&$value) {
+                    $value = Inflector::id2camel($value);
+                });
+                $controller = implode('\\', $parts) . '\\' . $controller;
+            }
         }
-        $action = 'action' . ucfirst(array_pop($parts));
-        $controller = ucfirst(array_pop($parts)) . 'Controller';
-        if (!empty($parts)) {
-            array_walk($parts, function(&$value) {
-                $value = ucfirst($value);
-            });
-            $controller = implode('\\', $parts) . '\\' . $controller;
-        }
+        return [$controller, $action];
+    }
+
+    /**
+     * Get default action
+     * @return array
+     */
+    protected function getDefaultControllerAndAction() {
+        $controller = $this->defaultController;
+        $action = Controller::getActionMethod('');
         return [$controller, $action];
     }
 }
