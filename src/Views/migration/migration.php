59d4629aeae6edb22b02656b1c490d4560abeba4
digit <digit.vova@gmail.com>
2018-05-30 08:54:11 +0300

    Add migration views

---
+++ src/Views/migration/migration.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This view is used by Reaction/Console/controllers/MigrateController.php.
+ *
+ * The following variables are available in this view:
+ */
+/* @var $className string the new migration class name without namespace */
+/* @var $namespace string the new migration class namespace */
+
+echo "<?php\n";
+if (!empty($namespace)) {
+    echo "\nnamespace {$namespace};\n";
+}
+?>
+
+use Reaction\Db\Migration;
+use function Reaction\Promise\reject;
+
+/**
+ * Class <?= $className . "\n" ?>
+ */
+class <?= $className ?> extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "<?= $className ?> cannot be reverted.\n";
+
+        return reject(new \Exception("Can not revert migration"));
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "<?= $className ?> cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
