316b43d58ad2b76fe07307920dc827946f00aa08
digit <digit.vova@gmail.com>
2018-05-30 12:20:44 +0300

    Console\Routes\Router update

--- src/Console/Routes/Router.php
+++ src/Console/Routes/Router.php
@@ -4,9 +4,7 @@ namespace Reaction\Console\Routes;
 
 use FastRoute\Dispatcher;
 use Reaction;
-use Reaction\Helpers\Inflector;
 use Reaction\RequestApplicationInterface;
-use Reaction\Routes\Controller;
 use Reaction\Routes\RouterAbstract;
 use Reaction\Routes\RouterInterface;
 
@@ -24,7 +22,7 @@ class Router extends RouterAbstract implements RouterInterface
     /**
      * @var string Default controller if none matches
      */
-    public $defaultController = 'Reaction\Console\Controllers\DefaultController';
+    public $defaultController = 'Reaction\Console\Controllers\HelpController';
 
     /**
      * Register all defined routes in dispatcher
@@ -41,79 +39,7 @@ class Router extends RouterAbstract implements RouterInterface
     public function getDispatcherData(RequestApplicationInterface $app)
     {
         $path = (string)$app->reqHelper->getPathInfo();
-        list($controllerName, $actionName) = $this->getControllerFromPathInfo($path);
-        list($controller, $actionName) = $this->searchController($controllerName, $actionName);
-        if (null === $controller) {
-            list($controller, $actionName) = $this->getDefaultControllerAndAction(true);
-        }
+        list($controller, $actionName) = $this->createController($path, ['app' => $app]);
         return [Dispatcher::FOUND, [$controller, $actionName]];
-    }
-
-    /**
-     * Search controller by name and existing method
-     * @param string $controllerName
-     * @param string $actionName
-     * @return array|null
-     */
-    protected function searchController($controllerName, $actionName)
-    {
-        if (!isset($controllerName) || !isset($actionName)) {
-            return [null, $actionName];
-        }
-        $controller = null;
-        $action = $actionName;
-        foreach ($this->controllerNamespaces as $namespace) {
-            $controllerNameFull = $namespace . '\\' . $controllerName;
-            if (!class_exists($controllerNameFull)) {
-                continue;
-            }
-            try {
-                $reflection = new \ReflectionClass($controllerNameFull);
-                $hasAction = $reflection->hasMethod($actionName);
-            } catch (\ReflectionException $exception) {
-                continue;
-            }
-            if(!$hasAction) {
-                $action = 'actionHelp';
-            }
-            $controller = Reaction::create($controllerNameFull);
-            break;
-        }
-        return [$controller, $action];
-    }
-
-    /**
-     * Get controller class name (not full) and action from path info
-     * @param string $path
-     * @return array with controller and action names [controller, action]
-     */
-    protected function getControllerFromPathInfo($path) {
-        $parts = explode('/', $path);
-        if (count($parts) < 2) {
-            return $this->getDefaultControllerAndAction();
-        } else {
-            $action = mb_strtolower(array_pop($parts));
-            $action = 'action' . Inflector::id2camel($action);
-            $controller = mb_strtolower(array_pop($parts));
-            $controller = Inflector::id2camel($controller) . 'Controller';
-            if (!empty($parts)) {
-                array_walk($parts, function(&$value) {
-                    $value = Inflector::id2camel($value);
-                });
-                $controller = implode('\\', $parts) . '\\' . $controller;
-            }
-        }
-        return [$controller, $action];
-    }
-
-    /**
-     * Get default action
-     * @param bool $instantiate
-     * @return array
-     */
-    protected function getDefaultControllerAndAction($instantiate = false) {
-        $controller = $instantiate ? Reaction::create($this->defaultController) : $this->defaultController;
-        $action = Controller::getActionMethod('');
-        return [$controller, $action];
     }
 }
