a8c908eecfe8110cad12c3faffa9a6cd6d9d5c7a
digit <digit.vova@gmail.com>
2018-05-30 12:19:57 +0300

    ControllerInterface update

--- src/Routes/ControllerInterface.php
+++ src/Routes/ControllerInterface.php
@@ -1,14 +1,26 @@
 <?php
 
 namespace Reaction\Routes;
+use Reaction\RequestApplicationInterface;
 
 /**
  * Interface ControllerInterface
  * @package Reaction\Routes
  * @property string $viewPath
+ * @property string $defaultAction
  */
 interface ControllerInterface
 {
+    /**
+     * @event ActionEvent an event raised right before executing a controller action.
+     * You may set [[$isValid]] to be false to cancel the action execution.
+     */
+    const EVENT_BEFORE_ACTION = 'beforeAction';
+    /**
+     * @event ActionEvent an event raised right after executing a controller action.
+     */
+    const EVENT_AFTER_ACTION = 'afterAction';
+
     /**
      * Routes description
      *
@@ -32,10 +44,38 @@ interface ControllerInterface
     public function group();
 
     /**
+     * Get controller ID
+     * @return string
+     */
+    public function getUniqueId();
+
+    /**
      * Register controller actions in router
      * @param Router $router
      */
     public function registerInRouter(Router $router);
+
+    /**
+     * Resolve controller action
+     * @param string                      $action
+     * @param RequestApplicationInterface $app
+     * @param mixed                       ...$params
+     * @return mixed
+     */
+    public function resolveAction(RequestApplicationInterface $app, string $action, ...$params);
+
+    /**
+     * Get actions list
+     * @return string[]
+     */
+    public function actions();
+
+    /**
+     * Check that controller has action with given ID
+     * @param string $actionId
+     * @return bool
+     */
+    public function hasAction($actionId);
 
     /**
      * Get path of action (Caution! With possible RegEx)
