1f8cc3da34e58760a178943826abeab2e3c9bee4
digit <digit.vova@gmail.com>
2018-06-08 15:27:49 +0300

    RequestAppComponent update

--- src/Base/RequestAppComponent.php
+++ src/Base/RequestAppComponent.php
@@ -2,14 +2,25 @@
 
 namespace Reaction\Base;
 
+use Reaction\Helpers\Request\HelpersGroup;
 use Reaction\RequestApplicationInterface;
 
 /**
  * Class RequestAppComponent
  * @package Reaction\Base
+ * @property HelpersGroup $hlp
  */
 class RequestAppComponent extends Component implements RequestAppComponentInterface
 {
     /** @var RequestApplicationInterface An request application instance reference */
     public $app;
+
+    /**
+     * Getter for $hlp
+     * @return HelpersGroup
+     */
+    protected function getHlp()
+    {
+        return $this->app->helpers;
+    }
 }
