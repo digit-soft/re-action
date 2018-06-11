9efe2839b8bee166d183fe54cbc4c6c7f28447b4
digit <digit.vova@gmail.com>
2018-06-11 13:01:47 +0300

    Console\Exception name update

--- src/Console/Exception.php
+++ src/Console/Exception.php
@@ -6,13 +6,13 @@ namespace Reaction\Console;
  * Class Exception
  * @package Reaction\Console
  */
-class Exception extends \Reaction\Exceptions\Exception
+class Exception extends \Reaction\Exceptions\UserException
 {
     /**
      * @return string the user-friendly name of this exception
      */
     public function getName()
     {
-        return 'Error';
+        return 'Console exception';
     }
 }
