f57a896a8f6b28d97c44a96c7aac0abf15d0abe7
digit <digit.vova@gmail.com>
2018-06-11 09:43:47 +0300

    I18n migration update

--- src/I18n/Migrations/m200418_153020_i18n_init.php
+++ src/I18n/Migrations/m200418_153020_i18n_init.php
@@ -1,43 +1,54 @@
 <?php
-//TODO: Come back after DB component development
 
-use yii\db\Migration;
+use Reaction\Db\ConnectionInterface;
+use Reaction\Db\Migration;
+use function Reaction\Promise\allInOrder;
 
 /**
  * Initializes i18n messages tables.
  */
 class m200418_153020_i18n_init extends Migration
 {
-    public function up()
+    /**
+     * @inheritdoc
+     */
+    public function safeUp(ConnectionInterface $connection)
     {
         $tableOptions = null;
-        if ($this->db->driverName === 'mysql') {
+        $promises = [];
+        if ($this->db->getDriverName() === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
-        $this->createTable('{{%source_message}}', [
+        $promises[] = $this->createTable('{{%source_message}}', [
             'id' => $this->primaryKey(),
             'category' => $this->string(),
             'message' => $this->text(),
         ], $tableOptions);
 
-        $this->createTable('{{%message}}', [
+        $promises[] = $this->createTable('{{%message}}', [
             'id' => $this->integer()->notNull(),
             'language' => $this->string(16)->notNull(),
             'translation' => $this->text(),
         ], $tableOptions);
 
-        $this->addPrimaryKey('pk_message_id_language', '{{%message}}', ['id', 'language']);
-        $this->addForeignKey('fk_message_source_message', '{{%message}}', 'id', '{{%source_message}}', 'id', 'CASCADE', 'RESTRICT');
-        $this->createIndex('idx_source_message_category', '{{%source_message}}', 'category');
-        $this->createIndex('idx_message_language', '{{%message}}', 'language');
+        $promises[] = $this->addPrimaryKey('pk_message_id_language', '{{%message}}', ['id', 'language']);
+        $promises[] = $this->addForeignKey('fk_message_source_message', '{{%message}}', 'id', '{{%source_message}}', 'id', 'CASCADE', 'RESTRICT');
+        $promises[] = $this->createIndex('idx_source_message_category', '{{%source_message}}', 'category');
+        $promises[] = $this->createIndex('idx_message_language', '{{%message}}', 'language');
+        return allInOrder($promises);
     }
 
-    public function down()
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(ConnectionInterface $connection)
     {
-        $this->dropForeignKey('fk_message_source_message', '{{%message}}');
-        $this->dropTable('{{%message}}');
-        $this->dropTable('{{%source_message}}');
+        $promises = [];
+        $promises[] = $this->dropForeignKey('fk_message_source_message', '{{%message}}');
+        $promises[] = $this->dropTable('{{%message}}');
+        $promises[] = $this->dropTable('{{%source_message}}');
+        return allInOrder($promises);
     }
 }
